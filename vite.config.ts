61a95a37a2e37d0c8526653b6a27f08e3b9da027
emagero0 <emagero0@gmail.com>
2025-02-27 12:46:34 +0300

    working copy

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  css: {
+    postcss: './postcss.config.cjs',
+  },
+  optimizeDeps: {
+    include: ['@tailwindcss/postcss7-compat']
+  }
 })
